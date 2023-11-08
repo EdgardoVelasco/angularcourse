05b56b2f065b119e2bf352f57b44f17deb8d8c17
edgardo <edgardo.nvf@gmail.com>
2023-11-08 11:26:02 -0600

    cambios

--- capitulo3/herencia.ts
+++ capitulo3/herencia.ts
@@ -4,7 +4,7 @@ class Electrodomestico{
     private _marca:string;
     private _precio:number;
 
-    constructor(nombre:string, marca:string, precio:number){
+    constructor(nombre:string="default", marca:string="default", precio:number=0){
         this._nombre=nombre;
         this._marca=marca;
         this._precio=precio;
@@ -19,3 +19,20 @@ class Electrodomestico{
     public get precio():number{return this._precio;}
     public set precio(precio:number){this._precio=precio;}
 }
+
+
+/*-*-*-*clase hija-*-*-*-*/
+class Lavadora extends Electrodomestico{
+    private _capacidad:number;
+    constructor(nombre:string, marca:string, precio:number, capacidad:number){
+        super(nombre, marca, precio);
+        this._capacidad=capacidad;
+    }
+    public get capacidad():number{return this._capacidad;}
+    public set capacidad(capacidad:number){this._capacidad=capacidad;}
+}
+
+console.log("-*-*Herencia-*-*-*");
+let lav1:Lavadora= new Lavadora("lava","GE", 10000, 19);
+console.log(`marca: ${lav1.marca}, precio: ${lav1.precio}, capacidad:${lav1.capacidad}`);
+

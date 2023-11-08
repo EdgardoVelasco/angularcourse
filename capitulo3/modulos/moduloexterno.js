10ce037ec2a15dfd0a046ce37a280827889cb9aa
edgardo <edgardo.nvf@gmail.com>
2023-11-08 13:09:00 -0600

    cambios

---
+++ capitulo3/modulos/moduloexterno.js
@@ -0,0 +1,24 @@
+"use strict";
+/*Módulo externo*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.nombre = exports.funA = exports.C = exports.A = void 0;
+var A = /** @class */ (function () {
+    function A() {
+    }
+    return A;
+}());
+exports.A = A;
+var B = /** @class */ (function () {
+    function B() {
+    }
+    return B;
+}());
+var C = /** @class */ (function () {
+    function C() {
+    }
+    return C;
+}());
+exports.C = C;
+function funA() { console.log("soy funcion A!!!"); }
+exports.funA = funA;
+exports.nombre = "edgar";
---
+++ capitulo3/modulos/moduloprincipal.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var moduloexterno_1 = require("./moduloexterno");
+var obj1 = new moduloexterno_1.A();
+var obj2 = new moduloexterno_1.C();
+(0, moduloexterno_1.funA)();
+console.log("nombre: ".concat(moduloexterno_1.nombre));
